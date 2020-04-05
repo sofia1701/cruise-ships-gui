af9c20b0f00cc18c7b671b4d8ae329d3583bf47d
Sofia <sofia.dionisio94@gmail.com>
2020-04-05 10:28:20 +0100

    Making tests DRY

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -1,41 +1,49 @@
 const { Port } = require("../src/index");
 
+describe('port', () => {
+    let port;
+    let ship;
+    let titanic;
+    let queenmary;
+    let diamondPrincess;
+
+    beforeEach(() => {
+        port = new Port('Sydney')
+        ship = {};
+        titanic = {};
+        queenmary = {};
+        diamondPrincess = {};
+    })
 
     describe('constructor function', () => {
         
         it('returns an object', () => {
-            const port = new Port('Sydney')
             expect(port).toBeInstanceOf(Object);
         })
         it('has a name', () => {
-            const port = new Port('Sydney')
             expect(port.portName).toBe('Sydney');
         })
     })
 
     describe('add ship', () => {
-        it('adds a ship to a port', () => {
-            const port = new Port('Sydney')
-            const ship = {};
+        it('adds a ship to a port', () => {            
             port.addShip(ship);
             expect(port.ships).toContain(ship);
         })
     })
     describe('remove ship', () => {
         it('removes a ship from a port', () => {
-            const port = new Port('Sydney')
-            const titanic = {};
-            const queenmary = {};
-            const diamondPrincess = {};
             port.addShip(titanic);
             port.addShip(queenmary);
             port.addShip(diamondPrincess)
             port.removeShip(queenmary);
 
             expect(port.ships).toEqual([titanic,diamondPrincess]);
-        })
-        
+        })   
     })
+})
+
+    
 
 
 
--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -2,56 +2,63 @@ const { Ship } = require("../src/index");
 const { Port } = require("../src/index");
 const { Itinerary } = require("../src/index");
 
-   let ship;
-   let port0;
-   let port1;
-   let itinerary;
-   
-   
-   beforeEach(() => {
-       port0 = new Port('Sydney');
-       port1 = new Port('Long Beach');
-       itinerary = new Itinerary([port0,port1]);
-       ship = new Ship(itinerary);    
-   });
+describe('ship', () => {
 
-   describe('constructor function', () => {
-       it('returns an object', () => {
-           expect(ship).toBeInstanceOf(Object);
-       })
-       it('has a starting port', () => {
-           expect(ship.currentPort).toBe(port0)
-       })
-   })
+    describe('with ports and an itinerary', () => {
+        let ship;
+        let port0;
+        let port1;
+        let itinerary;
+    
+        beforeEach(() => {
+            port0 = new Port('Sydney');
+            port1 = new Port('Long Beach');
+            itinerary = new Itinerary([port0,port1]);
+            ship = new Ship(itinerary);    
+        });
+    
+        describe('constructor function', () => {
+            it('returns an object', () => {
+                expect(ship).toBeInstanceOf(Object);
+            })
+            it('has a starting port', () => {
+                expect(ship.currentPort).toBe(port0)
+            })
+        })
+     
+        describe('set sail', () => {
+            it('able to set sail', () => {
+                ship.setSail()
+                expect(ship.currentPort).toBeFalsy();
+                expect(ship.previousPort).toStrictEqual(port0);
+                expect(port0.ships).not.toContain(ship);
+            })
+        })
+     
+        describe('dock', () => {
+            it('can dock to different ports', () => {
+                ship.setSail()
+                ship.dock()
+                expect(ship.currentPort).toBe(port1);
+                expect(port1.ships).toContain(ship)
+            })
+        })
+     
+        describe('throws an error', () => {
+            it('cant sail further than its itinerary', () => {
+                 ship.setSail()
+                 ship.dock() 
+                 expect(() => ship.setSail()).toThrowError('End of itinerary reached')
+            })
+        })
+     
+        describe('Ship > gets added to port on instantiation',() => {
+            it('gets added to port on instantiation', () => {
+                expect(port0.ships).toContain(ship)
+            })
+        })
+     
+    })
 
-   describe('set sail', () => {
-       it('able to set sail', () => {
-           ship.setSail()
-           expect(ship.currentPort).toBeFalsy();
-           expect(ship.previousPort).toStrictEqual(port0);
-           expect(port0.ships).not.toContain(ship);
-       })
-   })
+})
 
-   describe('dock', () => {
-       it('can dock to different ports', () => {
-           ship.setSail()
-           ship.dock()
-           expect(ship.currentPort).toBe(port1);
-           expect(port1.ships).toContain(ship)
-       })
-   })
-
-   describe('throws an error', () => {
-       it('cant sail further than its itinerary', () => {
-            ship.setSail()
-            ship.dock() 
-            expect(() => ship.setSail()).toThrowError('End of itinerary reached')
-       })
-   })
-
-   describe('Ship > gets added to port on instantiation',() => {
-       it('gets added to port on instantiation', () => {
-           expect(port0.ships).toContain(ship)
-       })
-   })
